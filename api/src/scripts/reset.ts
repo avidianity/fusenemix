b08ce346687f702aaafd98b70e7d17a6d7a5b0e6
John Michael Manlupig <manlupigjohnmichael@gmail.com>
2023-08-03 12:53:35 +0800

    Update

--- api/src/scripts/reset.ts
+++ api/src/scripts/reset.ts
@@ -13,7 +13,10 @@ async function main() {
 }
 
 main()
-  .catch(console.error)
-  .finally(() => {
+  .then(() => {
     process.exit(0);
+  })
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
   });
--- api/src/scripts/seed.ts
+++ api/src/scripts/seed.ts
@@ -33,7 +33,10 @@ async function main() {
 }
 
 main()
-  .catch(console.error)
-  .finally(() => {
+  .then(() => {
     process.exit(0);
+  })
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
   });
